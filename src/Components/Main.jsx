13451d2408580276c78d5a6614b712cc64c31f71
Aman Korabu <amankorabu24@gmail.com>
2025-03-14 11:19:34 +0530

    reverted the font change

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,7 @@
 
 import myImg from '../assets/photos/myImg.jpg'
 import Resume from '../assets/photos/Student Athlete Resume.pdf'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 function Main() {
   return (
@@ -19,13 +18,7 @@ function Main() {
             <div className="icons">
               <div style={{ display: "flex", gap: "15px", justifyContent: "center", padding: "20px" }}>
                 <a href="" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faInstagram} size="2x" style={{ color: "#E1308C" }} />
-                </a>
-                <a href="https://wa.me/91 8087115499" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faWhatsapp} size="2x" style={{ color: "#25D366" }} />
-                </a>
-                <a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" style={{ color: "#0077B5" }} />
+                 
                 </a>
               </div>
             </div>
